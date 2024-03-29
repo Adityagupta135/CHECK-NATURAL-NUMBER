ad26610fec5c8f397220c490ba72e5063d528b82
Adityagupta135 <146158454+Adityagupta135@users.noreply.github.com>
2024-03-29 11:56:40 +0530

    Add files via upload

---
+++ CHECK NATURAL NUMBER.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("enter n");
+	scanf("%d",&n);
+	if(n>1)
+	{
+		printf("natural number");
+	}
+	else {
+		printf("not a natural number");
+	
+	}
+	return 0;
+}
+
